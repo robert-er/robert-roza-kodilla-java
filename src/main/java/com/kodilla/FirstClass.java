cb8484e2caa9614e0d5cadb5744c6011ae6229d0
robert-er <robert.roza.pl@gmail.com>
2020-08-26 16:29:07 +0200

    extend FirstClass

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,14 @@
 package com.kodilla;
 
 public class FirstClass {
+
+    private String className;
+
+    public FirstClass(String className) {
+        this.className = className;
+    }
+
+    public String getClassName() {
+        return className;
+    }
 }
