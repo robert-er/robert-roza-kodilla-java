17394d0eef754445ebbcbb6f1577bca2342eca16
Daniel Oginski <daniel.oginski@jcommerce.pl>
2020-08-26 16:28:58 +0200

    added some code

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,34 @@
 package com.kodilla;
 
 public class FirstClass {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public FirstClass() {
+    }
+
+    @Override
+    public String toString() {
+        return "FirstClass{}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    
 }
